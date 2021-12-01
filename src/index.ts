db36db70d7567eefc617a4b0502907a3b0d4a03b
pixelzee3 <bpixelzee@gmail.com>
2021-12-01 17:43:52 +0700

    Added slash command functionality for test guilds.

--- src/index.ts
+++ src/index.ts
@@ -13,5 +13,21 @@ client.on('ready', () => {
 });
 
 client.on('messageCreate', message => {
+
 })
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'server') {
+		await interaction.reply('Server info.');
+	} else if (commandName === 'user') {
+		await interaction.reply('User info.');
+	}
+});
+
 client.login(token);
