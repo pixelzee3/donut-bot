215f179c25fa9ac8af3ac4712f16e0638f12cb94
pixelzee3 <bpixelzee@gmail.com>
2021-12-01 19:43:08 +0700

    Moved deploy commands to src folder

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -1,13 +1,13 @@
 const fs = require('fs');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const { clientId, guildId, token } = require(`${__dirname}/../../config/config.json`);
+const { clientId, guildId, token } = require(`${__dirname}/../config/config.json`);
 
 const commands = [];
-const commandFiles = fs.readdirSync(`${__dirname}`).filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync(`${__dirname}/commands`).filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-	const command = require(`${__dirname}/${file}`);
+	const command = require(`${__dirname}/commands/${file}`);
 	commands.push(command.data.toJSON());
 }
 const rest = new REST({ version: '9' }).setToken(token);
