7850377b24dd0f85aac20d7490fc22d080b6a9fc
pixelzee3 <bpixelzee@gmail.com>
2022-05-05 16:28:04 +0700

    Initialize TypeScript.

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,19 @@ const path = require('path');
 
 module.exports = {
   entry: {
-    main: './src/main.js',
+    main: './src/main.ts',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
   },
   output: {
     filename: '[name].js',
