92cfb98dc2469249c9020ca77f6368114468c070
pixelzee3 <bpixelzee@gmail.com>
2022-05-06 00:52:51 +0700

    Add env variables.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: {
@@ -20,4 +21,5 @@ module.exports = {
     filename: '[name].js',
     clean: true,
   },
+  plugins: [new Dotenv()],
 };
