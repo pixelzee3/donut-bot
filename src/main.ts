523ce22cd492d463df2a5c32315ecc2bf1c0a9c8
pixelzee3 <bpixelzee@gmail.com>
2022-05-07 17:57:43 +0700

    Add notes. Fundamental code rewrite is required, again.

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,26 @@
 // TODO: Read and execute external command files here and in deploy-commands.ts
 
 // Require the necessary discord.js classes
-import { Client, Intents } from 'discord.js';
+import fs from 'node:fs';
+import { Client, Collection, Intents } from 'discord.js';
 
-// Create a new client instance
+// Create a new client instance and command collections
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.ts'));
+
+// Loop over commandFiles and register them in commands
+// ! Looping over command files won't work with module bundlers. Must revert project to a normal TypeScript project without any module bundlers.
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  commands.set(command.name, command);
+}
 
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
   console.log('Ready!');
 });
+
 
 client.on('interactionCreate', async (interaction) => {
   if (!interaction.isCommand()) return;
