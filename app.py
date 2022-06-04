1e38f75efaf3a875637db08dd9fde30714a76d2a
pixelzee3 <bpixelzee@gmail.com>
2022-06-04 21:16:59 +0700

    Define logger.

--- app.py
+++ app.py
@@ -2,6 +2,14 @@
 from dotenv import load_dotenv
 import discord
 import os
+import logging
+
+# Define logger
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
 # Load environment variables
 try:
